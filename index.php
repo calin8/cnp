5745ce93bc9fb10724049c53c9bd610ff277b3c4
calin8 <32707132+calin8@users.noreply.github.com>
2021-08-17 10:22:22 +0300

    update verify function
    update verify function to return bool results and accept the cnp as parameter
    add option to launch the script from cli
    reformat code

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
+
 function create_form()
 {
 
-    $form = '
+	$form = '
 	<div>
         <form method="post" action="' . $_SERVER['PHP_SELF'] . '">
 			<input name="cnp" type="text">
@@ -10,80 +11,85 @@ function create_form()
         </form>
     </div>';
 
-    return $form;
+	return $form;
 }
 
-function validate_cnp()
+function validate_cnp($cnp)
 {
 
-    $cnp = trim($_POST['cnp']);
+	if (!$cnp) {
+		return false;
+	}
 
-    if (!$cnp)
-    {
-        return "CNP not valid - Error: Empty value";
-    }
+	if (strlen($cnp) != 13) {
+		return false;
+	}
 
-    if (strlen($cnp) != 13)
-    {
-        return "CNP not valid - Error: Invalid CNP lenght";
-    }
+	if (!is_numeric($cnp)) {
+		return false;
+	}
 
-    if (!is_numeric($cnp))
-    {
-        return "CNP not valid - Error: Invalid CNP not numeric";
-    }
+	if ($cnp[0] == 0) {
+		return false;
+	}
 
-    if ($cnp[0] == 0)
-    {
-        return "CNP not valid - Error: Invalid S";
-    }
+	if (($cnp[3] * 10 + $cnp[5]) > 12 || ($cnp[3] * 10 + $cnp[5]) == 0) {
+		return false;
+	}
 
-    if (($cnp[3] * 10 + $cnp[5]) > 12 || ($cnp[3] * 10 + $cnp[5]) == 0)
-    {
-        return "CNP not valid - Error: Invalid LL";
-    }
+	if (($cnp[5] * 10 + $cnp[6]) > 31 || ($cnp[5] * 10 + $cnp[6]) == 0) {
+		return false;
+	}
 
-    if (($cnp[5] * 10 + $cnp[6]) > 31 || ($cnp[5] * 10 + $cnp[6]) == 0)
-    {
-        return "CNP not valid - Error: Invalid ZZ";
-    }
+	if (($cnp[5] * 10 + $cnp[6]) > 52 || ($cnp[5] * 10 + $cnp[6]) == 52) {
+		return false;
+	}
 
-    if (($cnp[5] * 10 + $cnp[6]) > 52 || ($cnp[5] * 10 + $cnp[6]) == 52)
-    {
-        return "CNP not valid - Error: Invalid JJ";
-    }
+	$control_num = "279146358279";
+	$control_sum = 0;
 
-    $control_num = "279146358279";
-    $control_sum = 0;
+	for ($i = 0; $i < 12; $i++) {
+		$control_sum += $cnp[$i] * $control_num[$i];
+	}
 
-    for ($i = 0;$i < 12;$i++)
-    {
-        $control_sum += $cnp[$i] * $control_num[$i];
-    }
+	$control_check = $control_sum % 11;
 
-    $control_check = $control_sum % 11;
+	if ($control_check == 10 && $cnp[12] != 1) {
+		return false;
+	}
 
-    if ($control_check == 10 && $cnp[12] != 1)
-    {
-        return "CNP not valid - Error: Invalid C";
-    }
+	if ($control_check != $cnp[12]) {
+		return false;
+	}
 
-    if ($control_check != $cnp[12])
-    {
-        return "CNP not valid - Error: Invalid C";
-    }
-
-    return "CNP is valid";
+	return true;
 
 }
 
-if (!isset($_POST['cnp']))
+function get_args()
 {
-    echo create_form();
-}
-else
-{
-    echo validate_cnp();
+	if (isset($_SERVER['argv'][1])) {
+		return $_SERVER['argv'][1];
+	} else {
+		return false;
+	}
+
 }
 
-?>
+function get_post()
+{
+	if (isset($_POST['cnp'])) {
+		return trim($_POST['cnp']);
+	} else {
+		return false;
+	}
+
+}
+
+if ($cnp = get_args()) {
+	echo (validate_cnp($cnp)) ? "cnp is valid \n" : "cnp is not valid \n";
+} elseif ($cnp = get_post()) {
+	echo (validate_cnp($cnp)) ? "cnp is valid \n" : "cnp is not valid \n";
+} else {
+	echo create_form();
+}
